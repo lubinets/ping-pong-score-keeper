d5b130f62914820280e79aa049be68c63b1dc512
lubinets <lubineckarina@gmail.com>
2023-09-19 11:37:58 +0200

    Basic logic defined

---
+++ script.js
@@ -0,0 +1,30 @@
+const p1Point = document.querySelector("#p1Button");
+const p2Point = document.querySelector("#p2Button");
+
+const p1Display = document.querySelector(".score-player-1");
+const p2Display = document.querySelector(".score-player-2");
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
+
+p1Point.addEventListener("click", function () {
+  if (!isGameOver) {
+    p1Score += 1;
+    p1Display.textContent = p1Score;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+    }
+  }
+});
+
+p2Point.addEventListener("click", function () {
+  if (!isGameOver) {
+    p2Score += 1;
+    p2Display.textContent = p2Score;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+    }
+  }
+});
