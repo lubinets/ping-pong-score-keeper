105f32235dcbb910e867eb1bae2fc207029d1a38
lubinets <lubineckarina@gmail.com>
2023-09-20 13:34:13 +0200

    Visuals changed with Bulma UI

--- script.js
+++ script.js
@@ -23,8 +23,10 @@ p1Point.addEventListener("click", function () {
     p1Display.textContent = p1Score;
     if (p1Score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add("winner");
-      p2Display.classList.add("loser");
+      p1Display.classList.add("has-text-success");
+      p2Display.classList.add("has-text-danger");
+      p1Point.disabled = true;
+      p2Point.disabled = true;
     }
   }
 });
@@ -35,8 +37,10 @@ p2Point.addEventListener("click", function () {
     p2Display.textContent = p2Score;
     if (p2Score === winningScore) {
       isGameOver = true;
-      p2Display.classList.add("winner");
-      p1Display.classList.add("loser");
+      p2Display.classList.add("has-text-success");
+      p1Display.classList.add("has-text-danger");
+      p1Point.disabled = true;
+      p2Point.disabled = true;
     }
   }
 });
@@ -47,8 +51,10 @@ function reset() {
   p2Score = 0;
   p1Display.textContent = p1Score;
   p2Display.textContent = p2Score;
-  p2Display.classList.remove("winner", "loser");
-  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("has-text-success", "has-text-danger");
+  p1Display.classList.remove("has-text-success", "has-text-danger");
+  p1Point.disabled = false;
+  p2Point.disabled = false;
 }
 
 resetButton.addEventListener("click", reset);
